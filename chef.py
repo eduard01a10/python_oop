a37b07edaf0cd1ca30175e212a16bc66b3f2ec55
Eduardo Bautista Barrera <eg.bb952@gmail.com>
2020-12-09 21:20:25 -0600

    First structure of the project

---
+++ chef.py
@@ -0,0 +1,9 @@
+from person import Person
+
+class Chef(Person):
+
+    def __init__(self):
+        super().__init__()
+
+    def cook():
+        pass
---
+++ main.py
@@ -0,0 +1,4 @@
+##This is a basic OOP project with python
+
+if __name__ == "__main__":
+    pass
